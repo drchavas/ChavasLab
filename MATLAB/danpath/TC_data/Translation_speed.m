41d3fb184ed7b3ff9df2c2191305900917a50e55
Daniel Chavas <drchavas@gmail.com>
2019-01-09 08:45:48 -0500

    Uploading Python templates

--- MATLAB/danpath/TC_data/Translation_speed.m
+++ MATLAB/danpath/TC_data/Translation_speed.m
@@ -1,15 +1,16 @@
 %Translation_speed.m -- calculate TC translation velocity from track data
 %Purpose: calculate TC translation velocity at input time/location from
-%input track data
+%input track data on any sized spherical planet
 %
 % Syntax:  [u_translation,v_translation] = ...
-%                Translation_speed(lat_in,lon_in,t_tot_tc_in,t_interp)
+%                Translation_speed(lat_in,lon_in,t_tot_tc_in,t_interp,a_sphere)
 %
 % Inputs:
 %   lat_in - vector of track latitudes (any continuous format)
 %   lon_in - vector of track longitudes (any continuous format)
 %   t_tot_tc_in [day] - vector of track times (any single-number format)
 %   t_interp [day] - desired interpolation time (same format as t_tot_tc_in)
+%   a_sphere [m] - radius of planet (assumes sphere); default is 6371.22 km
 %
 % Outputs:
 %   u_translation - x-direction translation speed [ms-1]
@@ -17,32 +18,48 @@
 %
 % Example: 
 %	[u_translation v_translation] = Translation_speed(Lat_BT_TC,Lon_BT_TC,...
-%       tt_daysince1858111700UTC_TC,qs_tt_daysince1858111700UTC);
+%       tt_daysince1858111700UTC_TC,qs_tt_daysince1858111700UTC,a_sphere);
+%
+% Warning: this method will yield zero speed at transition step between
+% two periods at fixed position (e.g. 30N, 30N, 31N, 31N)
 %
 % Other m-files required: none
 % Subfunctions: spline
 % MAT-files required: none
 %
 % Author: Dan Chavas
-% CEE Dept, Princeton University
+% Purdue EAPS
 % email: drchavas@gmail.com
 % Website: -
-% 4 Dec 2013; Last revision: 23 Oct 2014
+% 4 Dec 2013; Last revision: 26 Oct 2018
 
 % Revision history:
 % 13 Jan 2014: changed spline() to pchip()
 % 23 Oct 2014: vectorized (a single period added to u_translation)
 % 06 Apr 2017: switched pchip to interp1(...,'pchip',NaN) to avoid
 %   extrapolation
+% 26 Oct 2018: added option to input a_sphere for calculation on any size
+%   planet
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-function [u_translation,v_translation] = Translation_speed(lat_in,lon_in,t_tot_tc_in,t_interp)
+function [u_translation,v_translation] = Translation_speed(lat_in,lon_in,t_tot_tc_in,t_interp,a_sphere)
+
+%% Check if a_sphere given or not
+switch nargin
+	case 4 % a_sphere has NOT been input, assume an average Earth radius
+        a_sphere = 6371.22*1000;    %[m]; CESM value
+        sprint('a_sphere argument not input and thus is set to average Earth value of %5.2f km',a_sphere/1000)
+	case 5 % a_sphere has been input
+	otherwise % options as <name-value> pairs
+		error('Incorrect number of arguments')
+end
 
 %% Interpolate translation speeds to desired time
 
 %set any constants
-km_per_deg=111.325; %[km/deg]
+% km_per_deg=111.325; %[km/deg]
+km_per_deg = (2*pi*a_sphere/360)/1000;   %[km]
 
 %%Lat (y) NOTE: MUST DO LAT FIRST!
 t = t_tot_tc_in; y = lat_in;
@@ -68,8 +85,8 @@ if(~isnan(lat_TC))
     %evaluate the derivative at the desired time (d(lat)/dt)
     dlat_dt=ppval(pp,t_interp);
 
-    %calculate meridional speed [m/s]: v=(d(lat)/dt) x (111.325 km/deg)
-    v_translation=dlat_dt*km_per_deg;   %[km/hr]
+    %calculate meridional speed [m/s]: v=(d(lat)/dt) x a_sphere
+    v_translation=dlat_dt*km_per_deg;   %[km/day]
     v_translation=v_translation*1000/(24*60*60);   %[m/s]
 
     clear pp A dlat_dt
@@ -95,7 +112,7 @@ if(~isnan(lat_TC))
     %evaluate the derivative at the desired time (d(lon)/dt)
     dlon_dt=ppval(pp,t_interp);
 
-    %calculate zonal speed [m/s]: u=(d(lon)/dt) x (111.325 km/deg)*(cos(lat))
+    %calculate zonal speed [m/s]: u=(d(lon)/dt) x (a_sphere)*(cos(lat))
     u_translation=dlon_dt.*km_per_deg.*cosd(lat_TC);
     u_translation=u_translation*1000/(24*60*60);   %[m/s]
 else
